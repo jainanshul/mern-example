ec79c278ced240190c226941eae0b1e685c1ef4e
Anshul Jain <aajain@apple.com>
2019-01-07 10:50:29 -0800

    Lint webpacke.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
+/* eslint-disable */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-enable */
+
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-    template: path.join(__dirname, 'public/index.html'),
-    filename: './index.html'
+  template: path.join(__dirname, 'public/index.html'),
+  filename: './index.html'
 });
 
-module.exports = {
+module.exports = { // eslint-disable-line
   entry: path.join(__dirname, 'src/index.js'),
   module: {
     rules: [
