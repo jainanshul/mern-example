69029f458d91fa67c8715117da30fb17f58061ba
Anshul Jain <aajain@apple.com>
2019-01-30 21:16:48 -0800

    Add an admin panel

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
+import {
+  Button,
+  Col,
+  Container,
+  Row,
+  Table,
+} from 'reactstrap';
 
 import Header from './Header';
+import user from './User';
 
 class Home extends Component {
   constructor(props) {
@@ -9,15 +17,62 @@ class Home extends Component {
 
     this.state = {
       error: '',
+      users: null,
     };
   }
 
+  handleAdmin = async () => {
+    try {
+      const users = await user.getAllUsers();
+      this.setState({users: users});
+    } catch(error) {
+      this.setState({
+        error: error.message,
+      });
+    }
+  }
+
   render() {
-    const {error} = this.state;
+    const {error, users} = this.state;
+
+    const userList = users ? users.map((user, i) => (
+      <tr key={i}>
+        <td>{user.email}</td>
+        <td>{user.loginAttempts}</td>
+        <td>{new Date(user.signUpDate).toDateString()}</td>
+        <td>{user.lockUntil ? new Date(user.lockUntil).toString() : ''}</td>
+      </tr>
+    )) : null;
 
     return (
       <div>
         <Header/>
+        <Container fluid style={{padding: 10}}>
+          <Row style={{padding: 10}}>
+            <Col>
+              <Button color="primary" onClick={this.handleAdmin}>Admin</Button>
+            </Col>
+          </Row>
+          {userList &&
+            <Row style={{padding: 10}}>
+              <Col>
+                <Table dark>
+                  <thead>
+                    <tr>
+                      <th>Email</th>
+                      <th>Login attempts</th>
+                      <th>Sign up date</th>
+                      <th>Locked until</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {userList}
+                  </tbody>
+                </Table>
+              </Col>
+            </Row>
+          }
+        </Container>
         {error &&
           <div className={'alert alert-danger'}>{error}</div>
         }
